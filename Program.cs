ea4ff4b755eaa3c1679274a0595e74893ef6a4c5
mlememm <yun.viviyet@gmail.com>
2025-06-15 15:23:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace InterfacesTask3
+{
+    class Vehicle
+    {
+        public string Name = "Загальний транспорт";
+    }
+    class Car : Vehicle
+    {
+        public string Model = "Легковий автомобіль";
+    }
+    interface IFactory<out T>
+    {
+        T Create();
+    }
+    class CarFactory : IFactory<Car>
+    {
+        public Car Create()
+        {
+            return new Car();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IFactory<Vehicle> vehicleFactory = new CarFactory();
+            Vehicle vehicle = vehicleFactory.Create();
+
+            Console.WriteLine("Об'єкт створено: " + vehicle.Name);
+
+            Console.ReadLine();
+        }
+    }
+}
